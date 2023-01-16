1f3e35e8ac6b3a45b6f211c78c640626fb96f6a4
Pushkar Singh <stoazest@gmail.com>
2023-01-16 12:53:24 +0530

    env variables name changed

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 const developmentSocket = "http://localhost:8080/";
 const developmentOrigin = "http://localhost:3000/";
 
-export const socketInUse = process.env.SOCKET || developmentSocket;
-export const originInUse = process.env.ORIGIN || developmentOrigin;
+export const socketInUse = process.env.REACT_APP_SOCKET || developmentSocket;
+export const originInUse = process.env.REACT_APP_ORIGIN || developmentOrigin;
 
 // stopwatch for the timer
 export const seconds = 20;
