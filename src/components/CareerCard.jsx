fb489444e5643c17ea9fb2b49e0be93b57cc172d
Pushkar Singh <stoazest@gmail.com>
2023-01-23 23:25:48 +0530

    career card ui update

--- src/components/CareerCard.jsx
+++ src/components/CareerCard.jsx
@@ -1,38 +1,47 @@
+function CareerCard({ name, id, changeChoice, numberOfPeople, currentChoice }) {
+  function choiceHandler(id) {
+    switch (id) {
+      case 1:
+        changeChoice(1);
+        break;
+      case 2:
+        changeChoice(2);
+        break;
+      case 3:
+        changeChoice(3);
+        break;
+      case 4:
+        changeChoice(4);
+        break;
+      default:
+        break;
+    }
+  }
 
-
-function CareerCard({name,id, changeChoice, numberOfPeople, currentChoice}){
-    function choiceHandler(id){
-        switch (id) {
-            case 1:
-                changeChoice(1)
-                break;
-            case 2:
-                changeChoice(2);
-                break;
-            case 3:
-                changeChoice(3);
-                break;
-        case 4:
-                changeChoice(4);
-                break;
-            default:
-                break;
-        }
-    };
-
-    return(
-        <div className="career-card">
-            {/* <code>{id}</code> */}
-            <h2>{name}</h2>
-            <p>{numberOfPeople ? `${numberOfPeople} have chosen this` : "0 have chosen this"}</p>
-        <button className={currentChoice === id ? "disabled" : "button"} onClick={() => {
-                if(currentChoice !== id){
-                    choiceHandler(id)
-                }
-            }}>{currentChoice ? currentChoice === id ? "Chosen" : "Change Career" : "Choose Career"}</button>
-        </div>
-    )
-};
-
+  return (
+    <div className="career-card">
+      <h2>{name}</h2>
+      <p>
+        {numberOfPeople
+          ? `${numberOfPeople} have chosen this`
+          : "0 have chosen this"}
+      </p>
+      <button
+        className={currentChoice === id ? "chosen-btn" : "button"}
+        onClick={() => {
+          if (currentChoice !== id) {
+            choiceHandler(id);
+          }
+        }}
+      >
+        {currentChoice
+          ? currentChoice === id
+            ? "Chosen"
+            : "Change Career"
+          : "Choose Career"}
+      </button>
+    </div>
+  );
+}
 
 export default CareerCard;
